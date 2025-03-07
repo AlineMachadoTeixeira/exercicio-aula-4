1d3922145f93addc15dc60109239a15e67ba3f03
Aline Teixeira <alinemachadoteixeira@gmail.com>
2025-03-07 20:04:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-import { somar } from "./somar";
-somar();
+import { somar } from "./somar.js";
+import { subtrair } from "./subtrair.js";
+import { dividir } from "./dividir.js";
+import { multiplicar } from "./multiplicar.js";
 
-import { subtrair } from "./subtrair";
-subtrair();
+const operacao = "+";
+let num1 = 10;
+let num2 = 5;
 
-import { dividir } from "./dividir";
-dividir();
+// Usando if
+let resultado = num1 + num2;
 
-import { multiplicar } from "./multiplicar";
-multiplicar();
+if (operacao == "+") {
+  resultado = somar(num1, num2);
+} else if (operacao == "-") {
+  resultado = subtrair(num1, num2);
+} else if (operacao == "*") {
+  resultado = multiplicar(num1, num2);
+} else if (operacao == "/") {
+  resultado = dividir(num1, num2);
+} else {
+  resultado = "Operação inválida!";
+}
+
+console.log(resultado);
+
+console.log("OI");
